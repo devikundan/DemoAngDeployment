ef544495081d4a77b2b0ffdda524b99890e5cfc9
devikundan <kundan825@gmail.com>
2026-04-19 22:30:16 +0530

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { DemoComponent } from './demo/demo.component';
 
 export const routes: Routes = [
+    { path: '', component: DemoComponent,pathMatch: 'full' },
    { path: 'demo', component: DemoComponent },
   {
     path: 'products',
